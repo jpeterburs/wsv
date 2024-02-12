297f47320cc1a2e6380c37bd6edb0282ccc33400
Jonas Peterburs <39838608+jpeterburs@users.noreply.github.com>
2024-02-12 10:02:37 +0100

    Use correct syntax for requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        'git+https://github.com/jpeterburs/reliable-txt.git'
+        'reliable-txt @ git+https://github.com/jpeterburs/reliable-txt.git'
     ]
 )
